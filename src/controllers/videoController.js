f5da1f39b0540c1794bfcce765fd31f54b02c542
indaKim <pinktail2327@naver.com>
2022-10-09 05:05:19 +0900

    check

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -56,6 +56,7 @@ export const postEdit = async (req, res) => {
   if (!video) {
     return res.status(403).render("404", { pageTitle: "Video not found." });
   }
+  console.log(video.owner, _id);
   if (String(video.owner) !== String(_id)) {
     req.flash("error", "You are not the owner of the video.");
     return res.status(403).redirect("/");
